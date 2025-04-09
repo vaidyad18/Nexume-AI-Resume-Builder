60014e5e5d3aa12599c2fb313b6a0b7de6a9bee7
Vaidya Dandriyal <vaidyadandriyal04@gmail.com>
2025-04-10 02:37:53 +0530

    key

--- service/GlobalApi.js
+++ service/GlobalApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_KEY = import.meta.env.VITE_STRAPI_API_KEY;
 const axiosClient = axios.create({
-    baseURL: "https://nexume-strapi-admin-2.onrender.com/api",
+    baseURL: import.meta.env.VITE_BASE_URL+"/api",
     headers: {
         "Content-Type": 'application/json',
         'Authorization': `Bearer ${API_KEY}`
